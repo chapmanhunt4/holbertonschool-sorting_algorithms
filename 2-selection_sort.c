c16816356b9e1123bb489b90d1db382e14b2150f
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-20 13:24:34 -0800

    moved print_array

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -20,10 +20,10 @@ void selection_sort(int *array, size_t size)
 		if (array[j] < array[min])
 		{
 			min = j;
+			print_array(array, size);
 		}
 	}
 	temp = array[min];
 	array[min] = array[i];
 	array[i] = temp;
-	print_array(array, size);
 }
