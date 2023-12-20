66a1d8fca4234ad87e652801f96cf152606232b1
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-20 13:22:04 -0800

    got called a suspect for spaces from copying an older version of my own code

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,21 +9,21 @@
 
 void selection_sort(int *array, size_t size)
 {
-	   size_t i, j, min, temp;
+	size_t i, j, min, temp;
 
-	   for (i = 0; i < size - 1; i++)
-	   {
-		   min = i;
-	   }
-	   for (j = i + 1; j < size; j++)
-	   {
-		   if (array[j] < array[min])
-		   {
-			   min = j;
-		   }
-	   }
-	   temp = array[min];
-	   array[min] = array[i];
-	   array[i] = temp;
-	   print_array(array, size);
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+	}
+	for (j = i + 1; j < size; j++)
+	{
+		if (array[j] < array[min])
+		{
+			min = j;
+		}
+	}
+	temp = array[min];
+	array[min] = array[i];
+	array[i] = temp;
+	print_array(array, size);
 }
