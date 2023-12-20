4d638df1081b0da05a8252ae5aed7250149f6709
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-20 13:15:05 -0800

    made selection sort function

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers using the 
+ * selection sort algorithm
+ * @array: an array of the integers given
+ * @size: size of the array
+ **/
+
+void selection_sort(int *array, size_t size)
+{
+	   size_t i, j, min, temp;
+
+	   for (i = 0; i < size - 1; i++)
+	   {
+		   min = i;
+	   }
+	   for (j = i + 1; j < size; j++)
+	   {
+		   if (array[j] < array[min])
+		   {
+			   min = j;
+		   }
+	   }
+	   temp = array[min];
+	   array[min] = array[i];
+	   array[i] = temp;
+	   print_array(array, size);
+}
