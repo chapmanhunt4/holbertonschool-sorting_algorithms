2a847c656b28f9512200968438d01ae8e9a8f851
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-20 08:07:47 -0800

    hopefully fix swap function

--- sort.h
+++ sort.h
@@ -26,6 +26,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void swap(int a, int b);
+void swap(int *a, int *b);
 
 #endif
