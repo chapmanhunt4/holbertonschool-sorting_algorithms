7f849bd0c99bf7b82e480b98174ab256566cc087
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-20 13:38:07 -0800

    fixed swap

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,6 +24,6 @@ void selection_sort(int *array, size_t size)
 	print_array(array, size);
 	}
 	temp = array[min];
-	array[min] = array[i];
-	array[i] = temp;
+	array[i] = array[min];
+	array[min] = temp;
 }
