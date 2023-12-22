5b809a8451a3ecbb42318abfb3fe46f9115f8d1a
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-21 19:22:47 -0800

    fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,21 +9,26 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min, temp;
+	size_t i, j, temp;
+	int min;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		min = i;
-	}
-	for (j = i + 1; j < size; j++)
-	{
-		if (array[j] < array[min])
+		min = array[i];
+		mindex = i;
+		for (j = i; j < size; j++)
 		{
-			min = j;
+			if (array[j] < array[mindex])
+			{
+				min = array[j];
+				mindex = j;
+			}
 		}
-	print_array(array, size);
+		if (mindex != i)
+		{
+			array[mindex] = array[i];
+			array[i] = min;
+			print_array(array, size);
+		}
 	}
-	temp = array[min];
-	array[min] = array[i];
-	array[i] = temp;
 }
